8175e57a541c63657adf1bd704d46dffa9cfcc22
icesnakeyang <flow_ocean@yahoo.com>
2019-12-22 13:29:47 +0800

    progress

--- src/pages/train/trainSettings.vue
+++ src/pages/train/trainSettings.vue
@@ -1,133 +1,157 @@
 <template>
   <div>
-    <Form :label-width="100">
-      <FormItem label="普通会员加价率">
-        <InputNumber :max="1" :min="0" :step="0.01" v-model="ticketRate"></InputNumber>
-        <span>100元火车票销售价：{{salesPrice0}}</span>
-      </FormItem>
-      <FormItem label="会员费1档">
-        <InputNumber :min="0" :step="1" v-model="memberFee1"></InputNumber>
-        <span>加价率</span>
-        <InputNumber :max="1" :min="0" :step="0.01" v-model="memberFee1Rate"></InputNumber>
-        <span>100元火车票销售价：{{salesPrice1}}</span>
-      </FormItem>
-      <FormItem label="会员费2档">
-        <InputNumber :min="0" :step="1" v-model="memberFee2"></InputNumber>
-        <span>加价率</span>
-        <InputNumber :max="1" :min=0 :step="0.01" v-model="memberFee2Rate"></InputNumber>
-        <span>100元火车票销售价：{{salesPrice2}}</span>
-      </FormItem>
-      <Button type="primary" @click="saveSettings">保存</Button>
-    </Form>
+    <div style="padding-top: 20px">
+      <Card>
+        <p slot="title">普通用户价格</p>
+        <Form :label-width="100">
+          <FormItem label="加价率">
+            <InputNumber :max="1" :min="0" :step="0.01" v-model="ticketRate"></InputNumber>
+            <span>100元火车票销售价：{{salesPrice0}}</span>
+          </FormItem>
+        </Form>
+      </Card>
+    </div>
+    <div style="padding-top: 20px">
+      <Card>
+        <p slot="title">会员1</p>
+        <Form :label-width="100">
+          <FormItem label="会员名称">
+            <Input type="text"></Input>
+          </FormItem>
+          <FormItem label="会员费">
+            <InputNumber :min="0" :step="1" v-model="memberFee1"></InputNumber>
+            <span>加价率</span>
+            <InputNumber :max="1" :min="0" :step="0.01" v-model="memberFee1Rate"></InputNumber>
+            <span>100元火车票销售价：{{salesPrice1}}</span>
+          </FormItem>
+        </Form>
+      </Card>
+    </div>
+    <div style="padding-top: 20px">
+      <Card>
+        <p slot="title">会员2</p>
+        <Form :label-width="100">
+          <FormItem label="会员名称">
+            <Input type="text"></Input>
+          </FormItem>
+          <FormItem label="会员费2档">
+            <InputNumber :min="0" :step="1" v-model="memberFee2"></InputNumber>
+            <span>加价率</span>
+            <InputNumber :max="1" :min=0 :step="0.01" v-model="memberFee2Rate"></InputNumber>
+            <span>100元火车票销售价：{{salesPrice2}}</span>
+          </FormItem>
+        </Form>
+      </Card>
+    </div>
+    <Button type="primary" @click="saveSettings">保存</Button>
   </div>
 </template>
 
 <script>
-    import {apiListTrainMemberKeys, apiUpdateTrainMemberSettings} from "../../api/api";
+  import {apiListTrainMemberKeys, apiUpdateTrainMemberSettings} from "../../api/api";
 
-    export default {
-        name: "trainSettings",
-        data() {
-            return {
-                ticketRate: 0.0,
-                basePrice: 100,
-                memberFee1: 15,
-                memberFee1Rate: 0.0,
-                memberFee2: 30,
-                memberFee2Rate: 0.0
+  export default {
+    name: "trainSettings",
+    data() {
+      return {
+        ticketRate: 0.0,
+        basePrice: 100,
+        memberFee1: 15,
+        memberFee1Rate: 0.0,
+        memberFee2: 30,
+        memberFee2Rate: 0.0
+      }
+    },
+    computed: {
+      salesPrice0() {
+        return parseFloat(this.basePrice * (1 + this.ticketRate)).toFixed(0)
+      },
+      salesPrice1() {
+        return parseFloat(this.basePrice * (1 + this.memberFee1Rate)).toFixed(0)
+      },
+      salesPrice2() {
+        return parseFloat(this.basePrice * (1 + this.memberFee2Rate)).toFixed(0)
+      }
+    },
+    methods: {
+      loadAllData() {
+        apiListTrainMemberKeys({}).then((response) => {
+          if (response.data.code === 0) {
+            let list = response.data.data.keys;
+            list.forEach((item, index) => {
+              if (item.keyName === 'ticketRate') {
+                this.ticketRate = Number(item.value)
+              }
+              if (item.keyName === 'memberFee1') {
+                this.memberFee1 = Number(item.value)
+              }
+              if (item.keyName === 'memberFee1Rate') {
+                this.memberFee1Rate = Number(item.value)
+              }
+              if (item.keyName === 'memberFee2') {
+                this.memberFee2 = Number(item.value)
+              }
+              if (item.keyName === 'memberFee2Rate') {
+                this.memberFee2Rate = Number(item.value)
+              }
+            })
+          } else {
+            this.$Message.error('读取会员配置信息失败')
+          }
+        }).catch((error) => {
+          this.$Message.error('读取会员配置信息失败')
+        })
+      },
+      saveSettings() {
+        this.$Modal.confirm({
+          title: '提问',
+          content: '<p>确定要修改所有的设置吗？</p>',
+          okText: '确定',
+          cancelText: '取消',
+          onCancel: () => {
+            return
+          },
+          onOk: () => {
+            let params = {
+              keys: [{
+                keyName: 'ticketRate',
+                keyValue: this.ticketRate,
+              },
+                {
+                  keyName: 'memberFee1',
+                  keyValue: this.memberFee1,
+                },
+                {
+                  keyName: 'memberFee1Rate',
+                  keyValue: this.memberFee1Rate,
+                },
+                {
+                  keyName: 'memberFee2',
+                  keyValue: this.memberFee2,
+                },
+                {
+                  keyName: 'memberFee2Rate',
+                  keyValue: this.memberFee2Rate,
+                }]
             }
-        },
-        computed: {
-            salesPrice0() {
-                return parseFloat(this.basePrice * (1 + this.ticketRate)).toFixed(0)
-            },
-            salesPrice1() {
-                return parseFloat(this.basePrice * (1 + this.memberFee1Rate)).toFixed(0)
-            },
-            salesPrice2() {
-                return parseFloat(this.basePrice * (1 + this.memberFee2Rate)).toFixed(0)
-            }
-        },
-        methods: {
-            loadAllData() {
-                apiListTrainMemberKeys({}).then((response) => {
-                    if (response.data.code === 0) {
-                        let list = response.data.data.keys;
-                        list.forEach((item, index) => {
-                            if (item.keyName === 'ticketRate') {
-                                this.ticketRate = Number(item.value)
-                            }
-                            if (item.keyName === 'memberFee1') {
-                                this.memberFee1 = Number(item.value)
-                            }
-                            if (item.keyName === 'memberFee1Rate') {
-                                this.memberFee1Rate = Number(item.value)
-                            }
-                            if (item.keyName === 'memberFee2') {
-                                this.memberFee2 = Number(item.value)
-                            }
-                            if (item.keyName === 'memberFee2Rate') {
-                                this.memberFee2Rate = Number(item.value)
-                            }
-                        })
-                    } else {
-                        this.$Message.error('读取会员配置信息失败')
-                    }
-                }).catch((error) => {
-                    this.$Message.error('读取会员配置信息失败')
-                })
-            },
-            saveSettings() {
-                this.$Modal.confirm({
-                    title: '提问',
-                    content: '<p>确定要修改所有的设置吗？</p>',
-                    okText: '确定',
-                    cancelText: '取消',
-                    onCancel: () => {
-                        return
-                    },
-                    onOk: () => {
-                        let params = {
-                            keys: [{
-                                keyName: 'ticketRate',
-                                keyValue: this.ticketRate,
-                            },
-                                {
-                                    keyName: 'memberFee1',
-                                    keyValue: this.memberFee1,
-                                },
-                                {
-                                    keyName: 'memberFee1Rate',
-                                    keyValue: this.memberFee1Rate,
-                                },
-                                {
-                                    keyName: 'memberFee2',
-                                    keyValue: this.memberFee2,
-                                },
-                                {
-                                    keyName: 'memberFee2Rate',
-                                    keyValue: this.memberFee2Rate,
-                                }]
-                        }
-                        apiUpdateTrainMemberSettings(params).then((response) => {
-                            if (response.data.code === 0) {
-                                this.$Message.success('保存成功')
-                            } else {
-                                    this.$Message.error('保存失败')
-                            }
-                        }).catch((error) => {
-                            this.$Message.error('保存失败')
-                        })
-                    },
-                });
-            }
-        },
-        mounted() {
-            this.loadAllData()
-        }
+            apiUpdateTrainMemberSettings(params).then((response) => {
+              if (response.data.code === 0) {
+                this.$Message.success('保存成功')
+              } else {
+                this.$Message.error('保存失败')
+              }
+            }).catch((error) => {
+              this.$Message.error('保存失败')
+            })
+          },
+        });
+      }
+    },
+    mounted() {
+      this.loadAllData()
     }
+  }
 </script>
 
 <style scoped>
-
 </style>
