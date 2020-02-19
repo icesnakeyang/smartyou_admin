7d2f7f52f6492f5ce2b4dda59ed7928070845a93
icesnakeyang <flow_ocean@yahoo.com>
2020-02-19 16:55:31 +0800

    progress

---
+++ src/pages/common/components/statusTagComponent.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div v-if="isTable"></div>
+    <div>
+      <div v-if="isActive">
+        <Tag color="success">已发布</Tag>
+      </div>
+      <div v-if="isReject">
+        <Tag color="error">已审核拒绝</Tag>
+      </div>
+      <div v-if="isPending">
+        <Tag color="warning">等待审核</Tag>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "statusTagComponent",
+        data() {
+            return {
+                status: '',
+                isPending: false,
+                isActive: false,
+                isReject: false
+            }
+        },
+        props: {
+            theStatus: '',
+            mode: false
+        },
+        computed: {
+            isTable() {
+                if (this.theStatus) {
+                    if (this.theStatus === 'ACTIVE') {
+                        this.isActive = true
+                    } else {
+                        if (this.theStatus === 'REJECT') {
+                            this.isReject = true
+                        }
+                        return 'ok'
+                    }
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/tour/tourList.vue
+++ src/pages/tour/tourList.vue
@@ -17,87 +17,102 @@
         <template slot-scope="{ row }" slot="name">
           <strong>{{ row.title }}</strong>
         </template>
+        <template slot-scope="{row}" slot="status">
+          <StatusTagComponent :the-status="row.status"></StatusTagComponent>
+        </template>
         <template slot-scope="{ row, index }" slot="action">
           <Button type="primary" size="small" style="margin-right: 5px" @click="btDetail(row)">详情</Button>
         </template>
       </Table>
+      <div style="margin-top: 20px">
+        <Page :total="totalTour" :page-size="pageSize" @on-change="changePage"></Page>
+      </div>
     </Card>
   </div>
 </template>
 
 <script>
-  import {apiListSpecialPriceTour, apiListTour} from "../../api/api";
+    import {apiListSpecialPriceTour, apiListTour} from "../../api/api";
+    import StatusTagComponent from '../common/components/statusTagComponent'
 
-  export default {
-    name: "tourList",
-    data() {
-      return {
-        tourList: [],
-        colListTour: [
-          {
-            title: '线路名称',
-            slot: 'name'
-          },
-          {
-            title: '简介',
-            key: 'brief'
-          },
-          {
-            title: '价格',
-            key: 'price'
-          },
-          {
-            title: '状态',
-            key: 'status'
-          },
-          {
-            title: '发布时间',
-            key: 'publishTime'
-          },
-          {
-            title: 'Action',
-            slot: 'action',
-            width: 150,
-            align: 'center'
-          }
-        ],
-        pageIndex: 1,
-        pageSize: 20,
-        totalTour: 0
-      }
-    },
-    methods: {
-      loadAllData() {
-        let params = {
-          pageIndex: this.pageIndex,
-          pageSize: this.pageSize
+    export default {
+        name: "tourList",
+        components: {
+            StatusTagComponent
+        },
+        data() {
+            return {
+                tourList: [],
+                colListTour: [
+                    {
+                        title: '线路名称',
+                        slot: 'name'
+                    },
+                    {
+                        title: '简介',
+                        key: 'brief'
+                    },
+                    {
+                        title: '价格',
+                        key: 'price'
+                    },
+                    {
+                        title: '状态',
+                        slot: 'status'
+                    },
+                    {
+                        title: '发布时间',
+                        key: 'publishTime'
+                    },
+                    {
+                        title: 'Action',
+                        slot: 'action',
+                        width: 150,
+                        align: 'center'
+                    }
+                ],
+                pageIndex: 1,
+                pageSize: 10,
+                totalTour: 0
+            }
+        },
+        methods: {
+            loadAllData() {
+                let params = {
+                    pageIndex: this.pageIndex,
+                    pageSize: this.pageSize
+                }
+                apiListTour(params).then((response) => {
+                    if (response.data.code === 0) {
+                        this.tourList = response.data.data.tours
+                        this.totalTour = response.data.data.totalTour
+                    } else {
+                        throw new Error('读取数据错误')
+                    }
+                }).catch((error) => {
+                    this.$Message.error(error)
+                })
+            },
+            btDetail(row) {
+                this.$router.push({
+                    name: 'tourDetail',
+                    params: {
+                        tourId: row.tourId
+                    }
+                })
+            },
+            remove(index) {
+                this.data6.splice(index, 1);
+            },
+            changePage(page) {
+                this.pageIndex = page
+                this.loadAllData()
+            }
+        },
+        mounted() {
+            this.loadAllData()
         }
-        apiListTour(params).then((response) => {
-          if (response.data.code === 0) {
-            this.tourList = response.data.data.tours
-          } else {
-            throw new Error('读取数据错误')
-          }
-        }).catch((error) => {
-          this.$Message.error(error)
-        })
-      },
-      btDetail(row) {
-        this.$router.push({
-          name: 'tourDetail',
-          params: {
-            tourId: row.tourId
-          }
-        })
-      },
-      remove(index) {
-        this.data6.splice(index, 1);
-      }
-    },
-    mounted() {
-      this.loadAllData()
     }
-  }
 </script>
 
 <style scoped>
